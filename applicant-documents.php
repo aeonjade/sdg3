551fdaa1b8f9e9ccbfc8e45caacaab5d0beaed90
xJaDe16x <aeonjade.vicentuan16@gmail.com>
2025-05-09 02:06:29 +0800

    applicantPage 1by1

--- applicant-documents.php
+++ applicant-documents.php
@@ -119,7 +119,7 @@ $documents = getDocuments('applicantID = ?', [$applicantID]);
 </body>
 
 <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
-<script src="javascript/document-upload.js"></script>
+<script src="javascript/applicantPage.js"></script>
 <script src="javascript/index.js"></script>
 
 </html>
